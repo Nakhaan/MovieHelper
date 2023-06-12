6a1520bc963d4bd0b6647156cae951950a4d70b8
Adrien Narkiewicz <adrien.nkwz@gmail.com>
2023-06-12 23:43:30 +0200

    async shotlist

--- src/app/overview/preprod/shotlist/shotlist.component.ts
+++ src/app/overview/preprod/shotlist/shotlist.component.ts
@@ -7,6 +7,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { BehaviorSubject, Observable, combineLatestWith, map, of } from 'rxjs';
+
+interface Data {
+  col1: string;
+  col2: string;
+  col3: string;
+  col4: string;
+  col5: string;
+  col6: string;
+}
 
 @Component({
     selector: 'app-shotlist',
@@ -35,17 +45,33 @@ export class ShotlistComponent {
     { col1: 'Scène', col2: 'Plan', col3: 'Valeur de plan',col4: 'Angle', col5: 'Mouvement', col6: 'Temps Estim.' },
   ];
 
+  protected refreshData$ = new BehaviorSubject<Data | null>(null);
+  protected data$: Observable<Data[]>;
+
   protected form: FormGroup = new FormGroup({
     col1: new FormControl(''),
     col2: new FormControl(''),
     col3: new FormControl(''),
     col4: new FormControl(''),
-    col5: new FormControl(''),
+    col5: new FormControl(''), 
     col6: new FormControl(''),
   });
 
+  public constructor() {
+    this.data$ = this.refreshData$.pipe(
+      combineLatestWith(of(this.data)),
+      map(([data, dataList]) => {
+        if(data) {
+          dataList.push(data);
+        }
+        return dataList;
+      })
+    )
+    ;
+  }
+
   protected addRow() {
-    this.data.push({ ...this.form.value });
+    this.refreshData$.next({ ...this.form.value })
     this.form.reset();
   }
 
